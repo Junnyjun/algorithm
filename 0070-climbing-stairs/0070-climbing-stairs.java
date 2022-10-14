0fefd71f02a52287c2ec9429c114d50ce137f150
Junny <72375574+Junnyjun@users.noreply.github.com>
2022-10-14 17:44:25 +0900

    Time: 0 ms (100.00%), Space: 41.3 MB (13.09%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+                // simple fibonacci problem
+        int steps=0;
+        int n1=0;
+        int n2=1;
+            for(int i=1; i<=n;i++){
+                steps=n1+n2;
+                n1=n2;
+                n2=steps;
+            }
+        return steps;
+    }
+}
