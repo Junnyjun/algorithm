87b53597237a48ec866b039322a8bb5ee8211518
Junny <72375574+Junnyjun@users.noreply.github.com>
2022-10-24 14:14:45 +0900

    Time: 165 ms (81.25%), Space: 35.1 MB (15.00%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun convertToTitle(columnNumber: Int): String {
+        val builder = StringBuilder()
+        var number = columnNumber
+        while (number > 0) {
+            builder.append('A' + (number - 1) % 26)
+            number = (number - 1) / 26
+        }
+        return builder.reversed().toString()
+    }
+
+
+}
