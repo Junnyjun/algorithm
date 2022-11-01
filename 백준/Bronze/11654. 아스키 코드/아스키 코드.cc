2c28a5888ce6cfd840332d386bc9fd2bb6558dd6
Junny <72375574+Junnyjun@users.noreply.github.com>
2022-11-01 14:08:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <stack>
+
+using namespace std;
+
+
+int main()
+{
+	
+	char c;
+	
+	scanf("%c",&c);
+	
+	printf("%d",c);
+	
+	
+	return 0;	
+}
+
