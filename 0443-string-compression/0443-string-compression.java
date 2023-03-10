107f9fcd962e6b332b5c364d46b6f2a06ec6a1df
Junny <72375574+Junnyjun@users.noreply.github.com>
2023-03-10 14:35:00 +0900

    Time: 1 ms (95.99%), Space: 41.8 MB (94.76%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int compress(char[] chars) {
+        int i = 0, res = 0;
+        while (i < chars.length) {
+            int groupLength = 1;
+            while (i + groupLength < chars.length && chars[i + groupLength] == chars[i]) {
+                groupLength++;
+            }
+            chars[res++] = chars[i];
+            if (groupLength > 1) {
+                for (char c : Integer.toString(groupLength).toCharArray()) {
+                    chars[res++] = c;
+                }
+            }
+            i += groupLength;
+        }
+        return res;
+    }
+}
